fa7548ada6f041335ff341b2ea2267d58f68a4a6
francocontigo <ricardoaugustofranco@hotmail.com>
2024-05-27 23:42:24 -0300

    Project Structure

---
+++ lazydm/pages/2_🧙‍♂️_NPCs.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="NPCs Generator", page_icon="🧙‍♂️")
+
+st.markdown("# NPCs Generator Demo")
+st.sidebar.header("NPCs Generator Header")
---
+++ lazydm/🦥_Lazy_DM_Tools.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Lazy DM Tools",
+    page_icon="🦥",
+)
+
+st.write("# Welcome to Lazy DM Tools! 🦥")
+
+st.sidebar.success("TESTING MEU CHAPA.")
